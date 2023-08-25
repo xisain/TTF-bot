8a65d532af66fd8958f8cd316c2af4bce6bdea01
UJENK148 <husenabs232bgr@gmail.com>
2023-08-25 15:43:05 +0700

    push

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-
+# TTF-bot
+# TTF-bot git config --global user.email husenabs232bgr@gmail.com
+# TTF-bot
+# TTF-bot
---
+++ index.js
@@ -0,0 +1,12 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
