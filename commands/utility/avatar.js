c4a6e589b3e15b0931adfc8af037faae19fca99e
UJENK148 <husenabs232bgr@gmail.com>
2023-08-26 09:08:38 +0700

    new update

--- commands/utility/avatar.js
+++ commands/utility/avatar.js
@@ -1,13 +1,21 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('avatar')
-		.setDescription('Get the avatar URL of the selected user, or your own avatar.')
-		.addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show')),
-	async execute(interaction) {
-		const user = interaction.options.getUser('target');
-		if (user) return interaction.reply(`${user.username}'s avatar: ${user.displayAvatarURL()}`);
-		return interaction.reply(`Your avatar: ${interaction.user.displayAvatarURL()}`);
-	},
+    data: new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('Get the avatar URL of the selected user, or your own avatar.')
+        .addUserOption(option => option.setName('username').setDescription("The user's avatar to show")),
+
+    async execute(interaction) {
+        const user = interaction.options.getUser('username') || interaction.user;
+
+        const embed = new EmbedBuilder()
+            .setTitle(`${user.username}'s avatar`)
+            .setImage(user.displayAvatarURL())
+            .setColor('#00FF00') // You can customize the color
+			.setTimestamp()
+			.setFooter({ text: `requested by ${interaction.user.username}`, iconURL: `${interaction.user.displayAvatarURL()}` });
+
+        return interaction.reply({ embeds: [embed] });
+    },
 };
